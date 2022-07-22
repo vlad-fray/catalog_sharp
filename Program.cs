2bf7482aad8101bf794f3a88805db2439fbd0d7c
vladislav usachev <vladislav.usachev@webpp.ru>
2022-07-22 14:36:17 +0300

    Добавлены сущности DTO

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using System.Data.Common;
+using Catalog.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IItemsRepository, InMemItemsRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
