d425818e3fbec154101d9a221d9d451dbcfa7c78
vladislav usachev <vladislav.usachev@webpp.ru>
2022-07-25 10:50:05 +0300

    Добавлен мангодебил

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
-using System.Data.Common;
 using Catalog.Repositories;
+using Catalog.Settings;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
+// Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddSingleton<IItemsRepository, InMemItemsRepository>();
+builder.Services.AddSingleton<IMongoClient>(ServiceProvider => 
+{
+    var settings = builder.Configuration.GetSection(nameof(MongoDBSettings)).Get<MongoDBSettings>();
+    return new MongoClient(settings.ConnectionString);
+});
+builder.Services.AddSingleton<IItemsRepository, MongoDBItemsRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
